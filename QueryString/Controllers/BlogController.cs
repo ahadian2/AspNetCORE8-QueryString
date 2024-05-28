c39a2a818737cc1e4690b716a2129e3d62f6c395
Mohammadreza <ahadian2@gmail.com>
2024-05-28 20:00:07 +0330

    Add project files.

---
+++ QueryString/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QueryString.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            string name = HttpContext.Request.Query["name"].ToString();
+            string family = HttpContext.Request.Query["family"].ToString();
+            ViewBag.Name = name + " " + family;
+            return View();
+        }
+        public IActionResult index2(string name, string family)
+        {
+            if(name==null || family == null)
+            {
+                ViewBag.Name = "مقداری وارد نشده است.";
+                return View();
+            }
+            ViewBag.Name = name + " " + family;
+            return View();
+        }
+        public IActionResult index3(int id)
+        {
+            ViewBag.id= id;
+            return View();
+        }
+
+    }
+}
